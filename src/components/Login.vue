45600820c8a290d354f7cacf10f9907386fa5a14
mcgaixuexi <mcgaixuexi@qq.com>
2020-02-11 10:43:15 +0800

    第二次提交

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -31,8 +31,8 @@ export default {
     return {
       // 这是登录表单的数据绑定对象
       loginForm: {
-        username: 'zs',
-        password: '12345'
+        username: 'admin',
+        password: '123456'
       },
       // 这是表单的验证规则对象
       loginFormRules: {
@@ -53,10 +53,18 @@ export default {
     resetLoginForm () {
       this.$refs.loginFormRef.resetFields()
     },
-    login () {
-      this.$refs.loginFormRef.validate((valid) => {
-        if (!valid) return false
-        this.$http.post('login', this.loginForm)
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败！')
+        this.$message.success('登录成功')
+        // 1.将登录成功之后的 token 保存到客户端的 sessionStorage中
+        // 1.1项目中除了登录之外的其他API接口，必须在登录之后才能访问
+        // 1.2 token 只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
       })
     }
   }
